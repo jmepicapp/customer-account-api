ef73fc55f0e768f8c8529909fcea241f24a34d2d
José María Agüero <jmaguero92@gmail.com>
2021-02-17 07:09:34 +0100

    first commit

---
+++ src/main/java/com/jmaguero/customer/account/api/NewAccountApiApplication.java
@@ -0,0 +1,13 @@
+package com.jmaguero.customer.account.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewAccountApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewAccountApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jmaguero/customer/account/api/NewAccountApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmaguero.customer.account.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewAccountApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
